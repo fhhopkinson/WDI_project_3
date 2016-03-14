c7a6dda7d325b9e782458f29faca4605f1d4dedc
error25 <autovincelynch@gmail.com>
2016-03-14 15:15:46 +0000

    added image uploader and static route

--- app.js
+++ app.js
@@ -7,13 +7,26 @@ var expressLayouts = require('express-ejs-layouts');
 var mongoose = require('mongoose');
 var router = require('./config/routes');
 
+
+// 2 routes files, one for the API, one for the frontend
+var apiRoutes = require('./config/routes/api');
+var frontendRoutes = require('./config/routes/frontend');
+
 mongoose.connect('mongodb://localhost/green-app');
+
+// set up static views for index.html
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
-app.use('/', router);
+// hook up the routes
+app.use('/api', apiRoutes);
+app.get('/', frontendRoutes);
+
 
 app.listen(3000);
