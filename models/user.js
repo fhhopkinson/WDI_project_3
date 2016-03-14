8a579bb1e665c63a03d3e3bdb531682baec42fac
Oli Forster <oliforster798@gmail.com>
2016-03-14 15:01:57 +0000

    authentication

--- models/user.js
+++ models/user.js
@@ -9,4 +9,30 @@ var userSchema = mongoose.Schema({
    passwordHash: String
 });
 
+userSchema.virtual('password')
+  .set(function(password) {
+    this._password = password;
+    this.passwordHash = bcrypt.hashSync(this._password, bcrypt.genSaltSync(8));
+  });
+
+userSchema.virtual('passwordConfirmation')
+  .set(function(passwordConfirmation) {
+    this._passwordConfirmation = passwordConfirmation;
+  });
+
+userSchema.path('passwordHash')
+  .validate(function(passwordHash) {
+    if(!this._password) {
+      return this.invalidate('password', 'A password is required');
+    }
+    if(this._password !== this._passwordConfirmation) {
+      return this.invalidate('passwordConfirmation', 'Passwords do not match');
+    }
+  });
+
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt.compareSync(password, this.passwordHash);
+}
+
+
 module.exports = mongoose.model('User', userSchema);
