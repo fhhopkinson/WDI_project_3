4e8f0a705fdc2729e33bb8fb161ce7ef7f989d4e
Acacia Mack <acacialmack@gmail.com>
2016-03-15 11:03:35 -0400

    added side navbar and forms can post

--- config/routes/frontend.js
+++ config/routes/frontend.js
@@ -2,7 +2,5 @@ var router           = require('express').Router();
 var staticController = require('../../controllers/static');
 
 router.get('/', staticController.index)
-router.get('/login', staticController.login)
-router.get('/register', staticController.register);
 
 module.exports = router;
--- controllers/static.js
+++ controllers/static.js
@@ -2,18 +2,6 @@ function staticIndex(req, res) {
   return res.render('index');
 }
 
-function staticLogin(req, res) {
-  return res.render('login');
-}
-
-function staticRegister(req, res) {
-  return res.render('register');
-}
-
-
-
 module.exports = {
-  index: staticIndex,
-  login: staticLogin,
-  register: staticRegister
+  index: staticIndex
 };
