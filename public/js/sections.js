d359d8ab535651d91667893932ec35c5ff4f2bf2
error25 <autovincelynch@gmail.com>
2016-03-15 20:26:20 +0000

    merging with dev

--- public/js/sections.js
+++ public/js/sections.js
@@ -1,3 +1,4 @@
+
 $( document ).ready(function() {
     console.log( "Sections file loaded" );
 
@@ -5,7 +6,10 @@ $( document ).ready(function() {
 $("#hubs").on('click',projectIndex)
 // Hide all pages (<MAINS>)
 $("main").hide()
-
+$(".hubslist").on('click', 'img', function(){
+  //showProject(this.id);
+  projectShow(this.id);
+});
 
 function projectIndex(){
     $("#login").hide()
@@ -13,42 +17,43 @@ function projectIndex(){
     $(".projectIndex").show()
     initMap(); // create map
     populateMap() // marker map
+    getProjects();
   }
    
+
 
 function getProjects(){
   event.preventDefault();
   ajaxRequest('GET', "http://localhost:3000/api/projects", null, function(data){
     console.log(data);
+    console.log("Do I exist?");
     displayProjects(data);
   });
 }
 
-function displayProjects(data){
-data = data.projects;
 
-$.each(data, function( index, project ) {
-   $(".hubslist").append("<li>"+ project.title + project.desc + project.projectDate + '<img src="' + project.gallery[0] + '" width="200px"/>' + project.projectDesc + project.projectDate + project.projectType + project.lat + project.lng + project.attendees + project.addresslineOne + project.addresslineTwo + '</li>');
+function displayProjects(data){
+console.log("arrived in displayProjects - ready to print to page");
+$.each(data.projects, function( index, project ) {
+   $(".hubslist").append("<li id='" + project._id + "' >"+ project.title + project.desc + project.projectDate + '<img src="' + project.gallery[0] + '" width="200px" id="' + project._id + '" />' + project.projectDesc + project.projectDate + project.projectType + project.lat + project.lng + project.attendees + project.addresslineOne + project.addresslineTwo + '</li>');
   });
 }
 
 
-// function projectShow(project){
-//   event.preventDefault();
-//   ajaxRequest('GET', "http://localhost:3000/api/projects/" + project, null, function(data){
-//     console.log(data);
-//     displayEventpage(data);
-//   });
-// }
-// function displayEventpage(data){
-//   $(data).each(function( index, project){
+function projectShow(project){
+  event.preventDefault();
+  ajaxRequest('GET', "http://localhost:3000/api/projects/" + project, null, function(data){
+    console.log(data);
+    displayEventpage(data);
+  });
+}
 
-//     $("#project-image").attr("src",project.projectImage);
-//     $("#event-date").text(project.projectDate);
-//     $("#event-desc").text(project.projectDesc);
-//     $("#event-desc").append("<p>" + project.projectLat + "</p><p>"  + project.projectLng + "</p> ");
-//     });
-// }
+function displayEventpage(data){
+  $("main").hide()
+    $(data).each(function( index, project){
+    console.log(project);
+  });
+}
 
 
 }); // end of js
