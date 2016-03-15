1e0a8683564d9c002efb1e16db3afef0d71c0b42
Acacia Mack <acacialmack@gmail.com>
2016-03-15 08:30:49 -0400

    added login and register pure forms

--- config/routes/frontend.js
+++ config/routes/frontend.js
@@ -1,6 +1,8 @@
 var router           = require('express').Router();
 var staticController = require('../../controllers/static');
 
-router.get('/', staticController.index);
+router.get('/', staticController.index)
+router.get('/login', staticController.login)
+router.get('/register', staticController.register);
 
 module.exports = router;
--- controllers/static.js
+++ controllers/static.js
@@ -2,6 +2,18 @@ function staticIndex(req, res) {
   return res.render('index');
 }
 
+function staticLogin(req, res) {
+  return res.render('login');
+}
+
+function staticRegister(req, res) {
+  return res.render('register');
+}
+
+
+
 module.exports = {
-  index: staticIndex
+  index: staticIndex,
+  login: staticLogin,
+  register: staticRegister
 };
