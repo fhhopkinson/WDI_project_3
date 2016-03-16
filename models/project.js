588c57e745fe459292d27c27ebfc96753230aac7
Oli Forster <oliforster798@gmail.com>
2016-03-16 12:07:27 +0000

    updated project schema

--- models/project.js
+++ models/project.js
@@ -2,15 +2,15 @@ var mongoose      = require('mongoose');
 var Comment       = require('./comment');
 
 var projectSchema = mongoose.Schema({
-   title: String,
-   projectType: String,
+   title: { type: String, required: true },
+   projectType: { type: String, required: true },
    projectDate: Date,
    lat: String,
    lng: String,
    addresslineOne: String,
    addresslineTwo: String,
    postcode: String,
-   desc: String,
+   desc: { type: String, required: true },
    image: String,
    gallery: [],
    attendees: [{ type: mongoose.Schema.ObjectId, ref: 'User' }],
