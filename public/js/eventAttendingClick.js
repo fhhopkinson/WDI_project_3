7ccce45358c042336248226720934080699f303c
Fred Hopkinson <fhhopkinson@gmail.com>
2016-03-17 22:28:46 +0000

    change again

--- public/js/eventAttendingClick.js
+++ public/js/eventAttendingClick.js
@@ -1,28 +1,20 @@
 $( document ).ready(function() {
-    console.log( "eventAttendingClick file loaded" );
+ 
 
   $( ".eventAttendingYes" ).click(function() {
    attendingAjaxRoute(this.id); // i.e. projectId + currentuserid
   });
 
   attendingAjaxRoute = function(projectId){
-    console.log("ajaxin")
     $.ajax({
       method: "PUT",
       url: "/api/attending/" + projectId + "/" + getUser()._id
     }).done(function(data) {
-         console.log( data );
+         
          alert( "Cool your attendance was registered." );
     });
     projectShow(projectId);
   }
 
-
-  //   console.log(loggedInUserId);
-  //   $.get( "/api/attending/56e98a848e1f571d22ea5501/" + loggedInUserId + "", function( data ) {
-  //     console.log( data );
-  //     alert( "Cool your attendance was registered." );
-  //   });
-  // }
 
 });
